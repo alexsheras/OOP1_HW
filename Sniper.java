55bf9e1a6ad5cb24c81d9bd81268420c0e4d97c4
ALEX SHER <sheras85@mail.com>
2023-04-30 19:29:29 +0300

    герои

---
+++ Sniper.java
@@ -0,0 +1,20 @@
+public class Sniper extends BaseHero {
+    public Sniper( String name, int strength, int health ) {
+        super( name, strength, health );
+    }
+
+
+    public Sniper() {
+        super( "Sniper", 5, 30 );
+    }
+
+
+    public void aim() {
+        System.out.println(this.name + " is aiming the sniper rifle.");
+    }
+
+    
+    public void shoot() {
+        System.out.println(this.name + " is shooting the sniper rifle.");
+    }
+}
